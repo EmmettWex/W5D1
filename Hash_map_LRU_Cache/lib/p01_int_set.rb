383651a6fa5e16d5a6df9165a31bc030524a95cf
“Emmett <emmett.wechsler11@gmail.com>
2022-12-05 11:51:24 -0500

    Commit hash_map_lru_cache

---
+++ Hash_map_LRU_Cache/lib/p01_int_set.rb
@@ -0,0 +1,93 @@
+class MaxIntSet
+
+  attr_reader :store
+
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    if is_valid?(num)
+      @store[num] = true
+      validate!(num)
+    else
+      raise 'Out of bounds'
+    end
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    return true if num >= 1 && num <= 50
+    false
+  end
+
+  def validate!(num)
+    @store[num]
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
